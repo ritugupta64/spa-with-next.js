801901af69188e60945593ece1c018d9690ed7f5
ritugupta64 <31064674+ritugupta64@users.noreply.github.com>
2020-03-27 23:18:47 +0530

    feat(context):required-content
    1)fix warnings

--- pages/index.js
+++ pages/index.js
@@ -48,11 +48,51 @@ const Home = () => {
 
           <h2>There are four principles of accessibility, we called POUR</h2>
           <ol>
-            <li><strong>Perceivable -</strong> Perceivable states that users must be able to perceive it in some way, using one or more of their senses.</li>
-            <li><strong>Operable -</strong> User interface components and navigation must be operable. This means that users must be able to operate the interface</li>
-            <li><strong>Understandable -</strong> Understandable states that information and the operation of user interface must be understandable.</li>
-            <li><strong>Robust -</strong>Robust states that content must be robust enough that it can be interpreted reliably by a wide variety of user agents, including assistive technologies.</li>
+            <li>
+              <strong>Perceivable -</strong> Perceivable states that users must
+              be able to perceive it in some way, using one or more of their
+              senses.
+            </li>
+            <li>
+              <strong>Operable -</strong> User interface components and
+              navigation must be operable. This means that users must be able to
+              operate the interface
+            </li>
+            <li>
+              <strong>Understandable -</strong> Understandable states that
+              information and the operation of user interface must be
+              understandable.
+            </li>
+            <li>
+              <strong>Robust -</strong>Robust states that content must be robust
+              enough that it can be interpreted reliably by a wide variety of
+              user agents, including assistive technologies.
+            </li>
           </ol>
+          <h2>What can ARIA do?</h2>
+          <p>ARIA allows you to communicate to the screen readers:</p>
+          <ul>
+            <li>aria-hidden</li>
+            <li>aria-label</li>
+            <li>aria-labelledby</li>
+            <li>role</li>
+            <li>aria-describedby</li>
+          </ul>
+          <span>**Note:</span>
+          <ul>
+            <li>most the screenreader doesn't support aria-describedby</li>
+            <li>Don't Invent Roles</li>
+            <li>
+              One of the
+              strengths of the aria-labelledby attribute is that it allows you
+              to refer to multiple selections of text and apply them all
+              together to label an element. This can be especially useful for
+              labeling complex forms, such as those laid out in a grid in which
+              multiple labels can apply to a single form field.
+            </li>
+          </ul>
+          {/* <span id="males"> ... <span id="frank"> ... <span id="ranking"> 
+<input type="text" aria-labelledby="males frank ranking"></input> */}
         </div>
       </div>
     </Layout>

b4e547321e3ae5a092453d2c47d199a68a1e294b
ritugupta64 <31064674+ritugupta64@users.noreply.github.com>
2020-03-30 10:22:04 +0530

    fix(chrome-bug):accessibility
    1) fixes

--- pages/best-practice.js
+++ pages/best-practice.js
@@ -25,6 +25,7 @@ const semanticRules = () => {
         <h1 id="page-heading">
            Don't use role attribute with elements until and unless its not required
         </h1>
+        <p>Testing this navigation with Chrome Vox, we found that without an ARIA role attribute, the screen reader would not read any other aria attributes–for this reason, it seems ARIA roles are crucial for screen readers.</p>
         <nav className="navigation" aria-label="nav1" role="navigation">
           <a target="_blank" href="https://docs.google.com/spreadsheets/d/1q12cRzz7fUSOhzUuUC1C7hsh4O5mT4_tvcw6HG6iCzc/edit#gid=0">Accessibility Checklist</a>
           <a href="/css/">CSS</a>
@@ -34,7 +35,7 @@ const semanticRules = () => {
         <h2>if nav element is more than one</h2>
         <p>Then always use aria-label="unique-name"</p>
       
-        <nav className="navigation" aria-label="nav2">
+        <nav className="navigation" aria-label="nav2" role="navigation">
           <a href="/js/">JavaScript</a>
           <a href="/jquery/">jQuery</a>
         </nav>
